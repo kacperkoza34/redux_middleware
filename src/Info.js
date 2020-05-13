81df1893e6876234f19ae8cdb800f2c9efefb072
Kacper Koza <kacperkoza34@gmail.com>
2020-05-13 12:42:22 +0200

    Update info

--- src/Info.js
+++ src/Info.js
@@ -1,32 +1,43 @@
-import React from 'react';
-import './Info.css';
+import React from "react";
+import "./Info.css";
 
 const Info = () => {
   return (
-    <div className='infoWrapper'>
+    <div className="infoWrapper">
       <h2>About</h2>
       <p>
-        Based on <a href={'https://www.youtube.com/watch?v=5gl3cCB_26M'} target='blank'>
+        Based on{" "}
+        <a href={"https://www.youtube.com/watch?v=5gl3cCB_26M"} target="blank">
           advance redux patterns
         </a>
       </p>
 
+      <p>You can check all actions in console</p>
       <p>
-        You can check all actions in console. Redux-devtools-extension is added to devDependencies
+        Here is{" "}
+        <a
+          href={"https://github.com/kacperkoza34/redux_middleware"}
+          target="blank"
+        >
+          repo
+        </a>
       </p>
 
       <p>
-        Check my <a href={`${window.location}books`} target='blank'>data-base</a>. I use json-server to connect with fake API
+        Check my{" "}
+        <a href={`${window.location}books`} target="blank">
+          data-base
+        </a>
+        . I use json-server to connect with fake API
       </p>
 
-      <div className='photoWrapper'>
-        <div className='photo'>
-          <img src={'images/redux1.png'} alt='ups'/>
+      <div className="photoWrapper">
+        <div className="photo">
+          <img src={"images/redux1.png"} alt="ups" />
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default Info;
